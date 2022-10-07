b25e12910cb5eafe35826f364f8ce3205a3dd5df
Bennett Benedict <bennybenester@gmail.com>
2022-10-07 11:58:45 +0300

    Add interface for person request body

---
+++ src/shared/interfaces/person.interface.ts
@@ -0,0 +1,31 @@
+import { IsIn, IsNotEmpty } from 'class-validator';
+
+export class Parent {
+  @IsNotEmpty({ message: 'Missing parent unique identifier' })
+  id: string;
+
+  firstName: string;
+
+  middleName: string;
+
+  gender: 'Male' | 'Female';
+}
+
+export class PersonInput {
+  @IsNotEmpty({ message: 'First name cannot be empty' })
+  firstName: string;
+
+  middleName: string;
+
+  @IsNotEmpty({ message: 'Last name cannot be empty' })
+  lastName: string;
+
+  @IsNotEmpty({ message: 'First name cannot be empty' })
+  @IsIn(['Male', 'Female'])
+  gender: 'Male' | 'Female';
+
+  children: Parent[];
+
+  @IsNotEmpty({ message: 'Parent can not be empty' })
+  parent: Parent;
+}
