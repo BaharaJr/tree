8890a0736c86b2c250721800d4c6be8e5e441d6f
Bennett Benedict <bennybenester@gmail.com>
2022-10-07 10:06:29 +0300

    Add global modules exports to app entry module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { dbConfig } from './core/system/system.config';
+import { modules } from './modules/modules';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'postgres',
+      ...dbConfig,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    ...modules,
+  ],
   controllers: [AppController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpErrorFilter } from './core/interceptors/errors.interceptor';
 import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
+import { system } from './core/system/system.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpErrorFilter());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const PORT = process.env.PORT || 3000;
+  system();
   await app.listen(PORT);
   Logger.debug(`App listening on port: ${PORT}`, 'APP');
 }
